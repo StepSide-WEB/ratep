45e9a0354735d93b845972b47a0607e04dd03e84
StepSide-WEB <alex.urkevich.a@gmail.com>
2025-05-27 18:01:38 +0300

    New pages arrived

--- src/widgets/pages/products/electrical-equipment/index.tsx
+++ src/widgets/pages/products/electrical-equipment/index.tsx
@@ -3,6 +3,8 @@ import HeadingSmall from "@/shared/ui-kit/heading/heading-small"
 import SectionWrapper from "@/shared/ui-kit/wrappers/section-wrapper"
 import Image from "next/image"
 import Filters from "./filters"
+import { Link } from "react-transition-progress/next"
+import { categorySlugs } from "@/shared/lib/constants/slugs"
 
 const ElectricalEquipment = () => {
 	return (
@@ -24,8 +26,62 @@ const ElectricalEquipment = () => {
 				</div>
 			</SectionWrapper>
 			<Filters />
+			<section className='grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 mt-12 sm:mt-28 mb-14 border-t sm:border-t-0 h-[800px] sm:h-[444px]'>
+				{productLinks.map((item, index) => (
+					<Link
+						key={index}
+						href={item.href}
+						className='group flex flex-col justify-between hover:bg-accent p-6 sm:border-t border-r border-b w-full h-full hover:text-white transition-all duration-200'
+					>
+						<p className='text-xl md:text-2xl tracking-[-0.02em]'>{item.title}</p>
+						<p className='text-black-60 group-hover:text-white text-sm leading-none tracking-[-0.02em] transition-all duration-200'>
+							{item.description}
+						</p>
+					</Link>
+				))}
+			</section>
 		</>
 	)
 }
+
+const productLinks = [
+	{
+		href: `/products/${categorySlugs.interiors}`,
+		title: "Интерьеры",
+		description: <>Производство потолочной обшивки вагонов рельсового транспорта</>,
+	},
+	{
+		href: `/products/${categorySlugs.busbars}`,
+		title: "Шины и токоотводы",
+		description: (
+			<>
+				Электрооборудование
+				<br /> для локомотивостроения
+			</>
+		),
+	},
+	{
+		href: `/products/${categorySlugs.surplusSale}`,
+		title: "Продажа неликвидов",
+		description: (
+			<>
+				Продажа ТМЦ,
+				<br />
+				снятых с производства
+			</>
+		),
+	},
+	{
+		href: `/products/${categorySlugs.surplusSale}`,
+		title: "Телеком",
+		description: (
+			<>
+				Телекоммуникационные
+				<br />
+				стойки и шкафы
+			</>
+		),
+	},
+]
 
 export default ElectricalEquipment
